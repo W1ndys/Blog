38785826a823fb020f2120a33f2f0e369edac36e
W1ndys <w1ndys@outlook.com>
2024-05-16 12:39:20 +0800

    build script

---
+++ 一键生成静态文件.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# 颜色定义
+GREEN='\033[32m'
+RESET='\033[0m'
+
+# 提示性语句
+printf "${GREEN}INFO ${RESET} 正在执行 Hexo Clean...\n"
+
+# 执行 hexo clean
+hexo clean
+
+# 提示性语句
+printf "${GREEN}INFO ${RESET} Hexo Clean 完成\n"
+
+# 提示性语句
+printf "${GREEN}INFO ${RESET} 正在执行 Hexo Generate...\n"
+
+# 执行 hexo generate
+hexo generate
+
+# 提示性语句
+printf "${GREEN}INFO ${RESET} Hexo Generate 完成\n"
+
