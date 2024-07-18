b17abd2d42a0fe46d137bec26b4e8e6c3a33db81
W1ndys <w1ndys@outlook.com>
2024-07-18 12:56:18 +0800

    更新脚本

--- 一键生成静态文件.sh
+++ 一键生成静态文件.sh
@@ -23,3 +23,14 @@ hexo generate
 printf "${GREEN}INFO ${RESET} Hexo Generate 完成\n"
 
 explorer.exe public
+
+# 定义要复制的路径
+path_to_copy="/opt/1panel/apps/openresty/openresty/www/sites/easy-qfnu.top/index"
+
+printf "\n\n已复制路径到剪贴板： $path_to_copy\n"
+
+# 使用PowerShell将路径复制到剪贴板
+powershell.exe "Set-Clipboard -Value '$path_to_copy'"
+
+# 退出脚本
+exit
