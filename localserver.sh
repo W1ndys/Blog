f30838d66aa9ddf9f46368f23ffa6e07aea4c06d
W1ndys <w1ndys@outlook.com>
2025-04-09 18:57:08 +0800

    feat(localserver): 优化本地服务器启动流程
    在本地服务器启动时，调整了执行 hexo s 的位置，确保在
    浏览器打开后再启动服务器，并添加了相应的提示性语句，
    提升了用户体验和信息反馈。
    
    无其他备注。

--- localserver.sh
+++ localserver.sh
@@ -25,12 +25,6 @@ printf "${GREEN}INFO ${RESET} Hexo Generate 完成\n"
 # 提示性语句
 printf "${GREEN}INFO ${RESET} 正在执行 Hexo Server...\n"
 
-# 执行 hexo s
-hexo s
-
-# 提示性语句
-printf "${GREEN}INFO ${RESET} Hexo Server 已启动\n"
-
 # 浏览器打开
 if command -v xdg-open > /dev/null; then
     xdg-open http://localhost:4000 &
@@ -43,3 +37,10 @@ else
 fi
 
 printf "${GREEN}INFO ${RESET} 已尝试在浏览器中打开 http://localhost:4000\n"
+
+# 执行 hexo s
+hexo s
+
+# 提示性语句
+printf "${GREEN}INFO ${RESET} Hexo Server 已启动\n"
+
